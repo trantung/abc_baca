e36f748949050834665b48b86b33eb1bec32b5f0
nmd <you@example.com>
2017-03-02 15:16:24 +0700

    fix

--- app/views/site/type.blade.php
+++ app/views/site/type.blade.php
@@ -39,10 +39,11 @@
 				@foreach($posts as $value)
 				<div class="grid_4">
 					<div class="post-item">
-						<a href="{{ url($value->slug) }}">
+						<a href="{{ url($value->slug) }}" class="overlay">
 							<img src="{{ url($value->image) }}">
 							<div class="post-label">
 								<span>{{ $value->name }}</span>
+								<p>{{ $value->summary }}</p>
 							</div>
 						</a>
 					</div>
